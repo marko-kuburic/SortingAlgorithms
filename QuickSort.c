d42468e2be660b55c92c6c44a7afe5a056e08581
Marko Kuburić <44305151+marko-kuburic@users.noreply.github.com>
2019-01-08 13:01:29 +0100

    linting step

--- QuickSort.c
+++ QuickSort.c
@@ -1,58 +1,54 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include < stdio.h > 
+#include < stdlib.h >
 
-void swap(int * i, int * j)
-{
-    int temp = *i;
-    *i = *j;
-    *j = temp;
-}
-void readData(int arr[], int n)
-{
-    for (int i = 0; i < n;i++)
-    {
+void swap(int * i, int * j) {
+    int temp = * i;
+    * i = * j;
+    * j = temp;
+    }
+void readData(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
         printf("\nEnter the value of %d. element: ", i);
-        scanf("%d", &arr[i]);
+        scanf("%d", & arr[i]);
     }
 }
-int partition(int arr[], int first, int last){
-        int pivot = arr[last];
-        int wall = (first - 1);
-        int i;
-        for(i = first; i <= last - 1; i++){
-            if(arr[i] <= pivot){
-                wall++;
-                swap(&arr[wall], &arr[i]);
-            }
+int partition(int arr[], int first, int last) {
+    int pivot = arr[last];
+    int wall = (first - 1);
+    int i;
+    for (i = first; i <= last - 1; i++) {
+        if (arr[i] <= pivot) {
+            wall++;
+            swap( & arr[wall], & arr[i]);
         }
-        swap(&arr[wall+1], &arr[last]);
-        return (wall + 1);
+    }
+    swap( & arr[wall + 1], & arr[last]);
+    return (wall + 1);
 }
-void quickSort(int arr[], int first, int last)
-{
-    if(first < last){
+void quickSort(int arr[], int first, int last) {
+    if (first < last) {
         int wall = partition(arr, first, last);
         quickSort(arr, first, wall - 1);
         quickSort(arr, wall + 1, last);
 
     }
 }
-void displayData(int arr[], int n)
-{
-    for (int i = 0; i < n; i++)
-    {
+void displayData(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
         printf("%d", arr[i]);
     }
 }
-int main()
-{
+int main() {
     int n, i;
     printf("Enter the lenght of array: ");
-    scanf("%d", &n);
+    scanf("%d", & n);
     int arr[n];
     readData(arr, n);
-    quickSort(arr, 0, n-1);
+    quickSort(arr, 0, n - 1);
     displayData(arr, n);
 
     return 0;
+}
+
+return 0;
 }
